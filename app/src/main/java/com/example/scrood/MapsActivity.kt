8f4d55c823d9b6e91ac3b2bb10e6e05aaeb8152c
maxoverhere <max@hunters.me.uk>
2020-06-18 11:35:34 +0200

    New change on maps for the current position

--- app/src/main/java/com/example/scrood/MapsActivity.kt
+++ app/src/main/java/com/example/scrood/MapsActivity.kt
@@ -1,11 +1,20 @@
 package com.example.scrood
 
+import android.app.DownloadManager
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.content.res.Resources
+import android.location.Location
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.internal.ResolveAccountRequest
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -13,16 +22,70 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_maps.*
+import java.util.jar.Manifest
 
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+
     private val TAG: String = MapsActivity::class.java.simpleName
-    private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+
+    private val REQUEST_CODE = 1000
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode
+        {
+            REQUEST_CODE->{
+            if (grantResults.size > 0)
+            {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    Toast.makeText(this@MapsActivity,"Permission granted",Toast.LENGTH_SHORT)
+                        .show()
+                else
+                    Toast.makeText(this@MapsActivity,"Permission denied",Toast.LENGTH_SHORT)
+                        .show()
+            }
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
+        //Check permission
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,android.Manifest.permission.ACCESS_FINE_LOCATION))
+            ActivityCompat.requestPermissions(this,arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),REQUEST_CODE)
+        else
+        {
+            buildLocationRequest ()
+            buildLocationCallBack ()
+
+            //Create FusedProviderClient
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+            //set event
+
+
+            if (ActivityCompat.checkSelfPermission(this@MapsActivity,android.Manifest.permission.ACCESS_FINE_LOCATION)  != PackageManager.PERMISSION_GRANTED &&
+                (ActivityCompat.checkSelfPermission(this@MapsActivity,android.Manifest.permission.ACCESS_COARSE_LOCATION)  != PackageManager.PERMISSION_GRANTED
+
+                {
+                   ActivityCompat.requestPermissions(this@MapsActivity, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, REQUEST_CODE))
+                    return@OnClickListener
+                }
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
+        }
+
 
         findViewById<ImageButton>(R.id.listBtn).setOnClickListener {
             val myIntent = Intent(this, ListActivity::class.java)
@@ -35,23 +98,20 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-        try {
-            val success = googleMap.setMapStyle(
-                MapStyleOptions.loadRawResourceStyle(
-                    this, R.raw.mapsconfig
-                )
-            )
-            if (!success) {
-                Log.e(TAG, "Style parsing failed.")
-            }
-        } catch (e: Resources.NotFoundException) {
-            Log.e(TAG, "Can't find style. Error: ", e)
-        }
+    private fun buildLocationCallBack() {
+       locationCallback = object :LocationCallback(){
 
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+           override fun onLocationResult(p0: LocationResult?) {
+               var location =p0!!.locations.get(p0!!.locations.size-1)
+               txt_location.text= location.latitude.toString()+"/"+location.longitude.toString()
+           }
+       }
     }
-}
+
+    private fun buildLocationRequest() {
+        locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 5000
+        locationRequest.fastestInterval = 3000
+        locationRequest.smallestDisplacement = 10f
+    }
